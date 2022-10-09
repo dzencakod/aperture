179774c49f79c9672d30faffc72aee3547297d20
Danil Dzenchako <dzencakod@gmail.com>
2022-10-09 15:59:15 +0300

    Finish to create site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.querySelector(".burger-span").addEventListener("click", () => {
 	document.querySelector("body").classList.toggle("active");
 	document.querySelector(".burger").classList.toggle("active");
+	document.querySelector(".burger-span").classList.toggle("active");
 })
